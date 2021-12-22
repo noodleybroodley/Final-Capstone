96a6a7fe4a958f07b38db7f003d780f9469ce175
Bradley Harrison <bradleyharrison@bradleys-mbp.home>
2021-12-22 18:00:47 -0500

    updated classses

--- src/clean_df.py
+++ src/clean_df.py
@@ -13,27 +13,27 @@ class DemographicDF(CleanDF):
 		self.df = pd.read_csv('../data/demographic.csv')[['SEQN','RIAGENDR','RIDAGEYR','RIDRETH3',
 															'DMQMILIZ','DMQADFC','DMDBORN4','DMDMARTL']]
 		self.column_dict = {
-			'RIAGENDR': {1.0: 'Male', 2.0: 'Female'},
+			'RIAGENDR': {1.0: 'Male', 2.0: 'Female', '.': 'Missing'},
 			'RIDRETH3': {1.0: 'Mexican American',
 						 2.0: 'Other Hispanic',
 						 3.0: 'White',
 						 4.0: 'Black',
 						 6.0: 'Asian',
-						 7.0: 'Other Race (or Multi-Racial'
+						 7.0: 'Other Race (or Multi-Racial',
+						 '.': 'Missing'
 						},
 			'DMQMILIZ': {1.0: 'Yes',2.0: 'No',7.0: 'Refused',9.0: "Don't Know",'.': 'Missing'},
 			'DMQADFC': {1.0: 'Yes',2.0: 'No',7.0: 'Refused',9.0: "Don't Know",'.': 'Missing'},
 			'DMDBORN4': {1.0: 'Born in US',2.0: 'Others',77.0: 'Refused',99.0: "Don't Know",'.': 'Missing'},
 			'DMDMARTL': {1.0: 'Married',2.0: 'Widowed',3.0: 'Divorced',4.0: 'Separated',5.0: 'Never married',6.0: 'Living w/ partner',77.0: 'Refused',99.0: "Don't Know",'.': 'Missing'}
 		}
-		self.clean_columns()
+		self.clean_data()
 
 
-	def clean_columns(self):
-		self.df = self.df.fillna(value='?')
+	def clean_data(self):
+		self.df = self.df.fillna(value='.')
 		def make_readable(self, column, x):
-			if x != '?':
-				return self.column_dict[column][x]
+			return self.column_dict[column][x]
 		for column in self.df.columns:
 			if column in self.column_dict:
 				self.df[column] = self.df[column].apply(lambda x: make_readable(self,column,x))
@@ -57,6 +57,24 @@ class LabsDF(CleanDF):
 		super(LabsDF,self).__init__()
 		self.df = pd.read_csv('../data/labs.csv')[['SEQN','LBXHCT','LBXTR']]
 
+class QuestDF(CleanDF):
+	def __init__(self):
+		super(QuestDF,self).__init__()
+		self.df = pd.read_csv('../data/questionnaire.csv')[['SEQN','BPQ020','BPQ080','BPQ040A','BPQ050A','BPQ090D']]
+		self.entry_dict = {1.0: 'Yes', 2.0: 'No', 7.0: 'Refused', 9.0: "Don't know", '.': 'Missing'}
+		self.clean_data()
+	
+	def clean_data(self):
+		self.df = self.df.fillna(value='.')
+		def make_readable(self, x):
+			try:
+				return self.entry_dict[x]
+			except KeyError:
+				return x
+		for column in self.df.columns:
+			self.df[column] = self.df[column].apply(lambda x: make_readable(self,x))
+	
 if __name__ == '__main__':
 	demo_df = DemographicDF()
 	exam_df = ExamDF()
+	ques_df = QuestDF()
