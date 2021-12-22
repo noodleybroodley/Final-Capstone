894f51be51472e2e0cfc95595966cbbc5959be19
Bradley Harrison <bradleyharrison@bradleys-mbp.home>
2021-12-22 14:52:16 -0500

    added more viz, improved classes

--- src/clean_df.py
+++ src/clean_df.py
@@ -4,8 +4,6 @@ import numpy as np
 class CleanDF(object):
 	def __init__(self):
 		self.df = pd.DataFrame()
-	# def join(self, other):
-	# 	self.df = self.df.join(other.df.set_index('SEQN'), on='SEQN')
 	
 	def left_merge(self, other, on='SEQN'):
 		self.df = self.df.merge(other.df, how='left', on=on)
@@ -18,9 +16,9 @@ class DemographicDF(CleanDF):
 			'RIAGENDR': {1.0: 'Male', 2.0: 'Female'},
 			'RIDRETH3': {1.0: 'Mexican American',
 						 2.0: 'Other Hispanic',
-						 3.0: 'Non-Hispanic White',
-						 4.0: 'Non-Hispanic Black',
-						 6.0: 'Non-Hispanic Asian',
+						 3.0: 'White',
+						 4.0: 'Black',
+						 6.0: 'Asian',
 						 7.0: 'Other Race (or Multi-Racial'
 						},
 			'DMQMILIZ': {1.0: 'Yes',2.0: 'No',7.0: 'Refused',9.0: "Don't Know",'.': 'Missing'},
@@ -45,15 +43,19 @@ class ExamDF(CleanDF):
 		super(ExamDF,self).__init__()
 		self.df = pd.read_csv('../data/examination.csv')[['SEQN','BPXSY1','BPXSY2','BPXSY3','BPXSY4','BPXDI1','BPXDI2','BPXDI3','BPXDI4']]
 		self.create_average_bp_columns()
+		self.drop_other_columns()
 	
 	def create_average_bp_columns(self):
 		self.df['AVG SYS BP'] = self.df[['BPXSY1','BPXSY2','BPXSY3','BPXSY4']].mean(axis=1)
 		self.df['AVG DIAS BP'] = self.df[['BPXDI1','BPXDI2','BPXDI3','BPXDI4']].mean(axis=1)
+	
+	def drop_other_columns(self):
+		self.df = self.df.drop(['BPXSY1','BPXSY2','BPXSY3','BPXSY4','BPXDI1','BPXDI2','BPXDI3','BPXDI4'],axis=1)
 
 class LabsDF(CleanDF):
 	def __init__(self):
 		super(LabsDF,self).__init__()
-		self.df = pd.read_csv('../data/labs.csv')[['SEQN','LBXHCT','LBXTC']]
+		self.df = pd.read_csv('../data/labs.csv')[['SEQN','LBXHCT','LBXTR']]
 
 if __name__ == '__main__':
 	demo_df = DemographicDF()
