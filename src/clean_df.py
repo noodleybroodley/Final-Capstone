ecbe085b1d3e38814b49f777106584ccd639cdfb
Bradley Harrison <bradleyharrison@Bradleys-MacBook-Pro.local>
2021-12-28 14:22:45 -0600

    added some logistic regressions

--- src/clean_df.py
+++ src/clean_df.py
@@ -46,10 +46,10 @@ class ExamDF(CleanDF):
 		self.drop_other_columns()
 	
 	def create_average_bp_columns(self):
-		self.df['AVG SYS BP'] = self.df[['BPXSY1','BPXSY2','BPXSY3','BPXSY4']].mean(axis=1)
-		self.df['AVG DIAS BP'] = self.df[['BPXDI1','BPXDI2','BPXDI3','BPXDI4']].mean(axis=1)
-		self.df['High SYS BP?'] = self.df['AVG SYS BP'].apply(lambda x: x >= 130)
-		self.df['High DIAS BP?'] = self.df['AVG DIAS BP'].apply(lambda x: x >= 80)
+		self.df['AVG_SYS_BP'] = self.df[['BPXSY1','BPXSY2','BPXSY3','BPXSY4']].mean(axis=1)
+		self.df['AVG_DIAS_BP'] = self.df[['BPXDI1','BPXDI2','BPXDI3','BPXDI4']].mean(axis=1)
+		self.df['High_SYS_BP'] = self.df['AVG_SYS_BP'].apply(lambda x: int(x >= 130))
+		self.df['High_DIAS_BP'] = self.df['AVG_DIAS_BP'].apply(lambda x: int(x >= 80))
 	
 	def drop_other_columns(self):
 		self.df = self.df.drop(['BPXSY1','BPXSY2','BPXSY3','BPXSY4','BPXDI1','BPXDI2','BPXDI3','BPXDI4'],axis=1)
